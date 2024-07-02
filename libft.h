9bc5d939e0649560185aefbcc603123679609ed6
Kazuki Takada <katakada@c3r3s8.42tokyo.jp>
2024-07-02 15:07:33 +0900

    Modification of Norm

--- libft.h
+++ libft.h
@@ -6,24 +6,23 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 17:13:09 by katakada          #+#    #+#             */
-/*   Updated: 2024/06/30 17:19:57 by katakada         ###   ########.fr       */
+/*   Updated: 2024/07/02 14:44:28 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <unistd.h>
-# include <stdlib.h>
 # include <limits.h>
-# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_list
 {
-    void			*content;
-    struct s_list	*next;
-}	t_list;
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
-int        ft_isalpha(int c);
+int					ft_isalpha(int c);
 
 #endif
