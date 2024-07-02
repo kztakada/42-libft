fc7c936d9e256813b216c1b71de24954a9ebb216
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-02 20:09:21 +0900

    feat: Add ft_strlcat function

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 18:07:16 by katakada          #+#    #+#             */
-/*   Updated: 2024/07/02 18:48:03 by katakada         ###   ########.fr       */
+/*   Updated: 2024/07/02 20:07:27 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dset, const void *src, size_t n)
 	unsigned char	*dst_ptr;
 	unsigned char	*src_ptr;
 
-	if (!dset && !src)
+	if (!dset || !src)
 		return (NULL);
 	dst_ptr = (unsigned char *)dset;
 	src_ptr = (unsigned char *)src;
---
+++ ft_strlcat.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/02 19:39:09 by katakada          #+#    #+#             */
+/*   Updated: 2024/07/02 20:06:08 by katakada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	if (!src)
+		return (0);
+	if (!dst && dstsize == 0)
+		return (ft_strlen(src));
+	if (!dst)
+		return (0);
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
+	i = 0;
+	while (src[i] && i < dstsize - dst_len - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
+}
