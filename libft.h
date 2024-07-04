8b7751a1af6132ff163dd6de3e63dde3484b489d
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-04 18:18:12 +0900

    Add ft_toupper function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 17:13:09 by katakada          #+#    #+#             */
-/*   Updated: 2024/07/02 19:55:19 by katakada         ###   ########.fr       */
+/*   Updated: 2024/07/04 17:04:07 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ void				*ft_memcpy(void *dset, const void *src, size_t n);
 void				*ft_memmove(void *dset, const void *src, size_t n);
 size_t				ft_strlcpy(char *dst, const char *src, size_t size);
 size_t				ft_strlcat(char *dst, const char *src, size_t dstsize);
+int					ft_toupper(int c);
 
 #endif
