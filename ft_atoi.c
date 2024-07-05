f15cb329cfd91412fb7ed064f70f08c488a840a7
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-05 22:05:46 +0900

    Refactor ft_atoi function for better performance to overflow

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,33 +6,48 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 20:08:39 by katakada          #+#    #+#             */
-/*   Updated: 2024/07/05 20:56:24 by katakada         ###   ########.fr       */
+/*   Updated: 2024/07/05 22:03:17 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static long	handle_overflow(const char *nptr, int sign)
+{
+	unsigned long	ret;
+	int				digit;
+	unsigned long	cutoff;
+
+	ret = 0;
+	cutoff = (unsigned long)LONG_MAX;
+	while (ft_isdigit(*nptr))
+	{
+		digit = *nptr - '0';
+		if (sign == 1 && ret > ((cutoff - digit) / 10))
+			return (LONG_MAX);
+		if (sign == -1 && ret > ((cutoff + 1 - digit) / 10))
+			return (LONG_MIN);
+		ret = ret * 10 + digit;
+		nptr++;
+	}
+	return (ret);
+}
+
 int	ft_atoi(const char *nptr)
 {
-	int			i;
-	int			sign;
-	long long	ret;
+	int		sign;
+	long	result;
 
-	i = 0;
 	sign = 1;
-	ret = 0;
-	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
-		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
+	result = 0;
+	while (*nptr == ' ' || (*nptr >= 9 && *nptr <= 13))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
 	{
-		if (nptr[i] == '-')
+		if (*nptr == '-')
 			sign = -1;
-		i++;
+		nptr++;
 	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		ret = ret * 10 + nptr[i] - '0';
-		i++;
-	}
-	return ((int)(ret * sign));
+	result = (int)handle_overflow(nptr, sign);
+	return ((int)(result * sign));
 }
