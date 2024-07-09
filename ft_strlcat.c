5c8dc438aa56c1c4c0efc8f1fb0fbfdca1bae791
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-09 17:31:55 +0900

    Refactor ft_strlcat function for consistency and readability

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 19:39:09 by katakada          #+#    #+#             */
-/*   Updated: 2024/07/02 20:06:08 by katakada         ###   ########.fr       */
+/*   Updated: 2024/07/09 16:20:03 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dst_len;
 	size_t	src_len;
@@ -20,16 +20,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	if (!src)
 		return (0);
-	if (!dst && dstsize == 0)
+	if (!dst && size == 0)
 		return (ft_strlen(src));
 	if (!dst)
 		return (0);
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	if (dstsize <= dst_len)
-		return (dstsize + src_len);
+	if (size <= dst_len)
+		return (size + src_len);
 	i = 0;
-	while (src[i] && i < dstsize - dst_len - 1)
+	while (src[i] && i < size - dst_len - 1)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
