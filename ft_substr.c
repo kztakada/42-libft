49a5ff8c0a41b69d9aadd4591acbcf0aea6e751e
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-11 18:03:21 +0900

    Refactor ft_substr function for improved performance and readability

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:49:05 by katakada          #+#    #+#             */
-/*   Updated: 2024/07/10 21:19:42 by katakada         ###   ########.fr       */
+/*   Updated: 2024/07/11 18:00:18 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	i;
+	size_t	s_len;
 
 	if (!s)
 		return (NULL);
-	if (ft_strlen(s) < start)
+	s_len = ft_strlen(s);
+	if (s_len < start)
 		return (ft_strdup(""));
-	if (ft_strlen(s) < start + len)
-		len = ft_strlen(s) - start;
+	if (s_len < start + len)
+		len = s_len - start;
 	substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!substr)
 		return (NULL);
