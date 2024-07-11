2933aeeb34128b812441cde44e3f000e6845c6e7
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-11 17:21:44 +0900

    Add ft_itoa function

---
+++ ft_itoa.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 17:50:21 by katakada          #+#    #+#             */
+/*   Updated: 2024/07/11 17:21:26 by katakada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_numlen(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+	long	nbr;
+
+	nbr = n;
+	len = ft_numlen(n);
+	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	str[len] = '\0';
+	if (nbr < 0)
+	{
+		str[0] = '-';
+		nbr *= -1;
+	}
+	if (nbr == 0)
+		str[0] = '0';
+	while (nbr)
+	{
+		str[--len] = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	return (str);
+}
