d1af399e559b821df1e9736ba408cc4e46562ace
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-11 16:26:49 +0900

    Add ft_strjoin function

---
+++ ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 17:49:25 by katakada          #+#    #+#             */
+/*   Updated: 2024/07/11 16:26:13 by katakada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	join = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (!join)
+		return (NULL);
+	ft_strlcpy(join, s1, len_s1 + 1);
+	ft_strlcat(join, s2, len_s1 + len_s2 + 1);
+	return (join);
+}
