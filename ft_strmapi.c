e8d80a7ade43177355e9726bc7003c7e888d3aec
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-11 18:29:44 +0900

    Add ft_strmapi function

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 17:50:35 by katakada          #+#    #+#             */
+/*   Updated: 2024/07/11 18:28:50 by katakada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	idx;
+	char			*str;
+
+	if (!s || !f)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (NULL);
+	idx = 0;
+	while (s[idx])
+	{
+		str[idx] = f(idx, s[idx]);
+		idx++;
+	}
+	str[idx] = '\0';
+	return (str);
+}
