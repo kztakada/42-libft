977b285b6f911ad9f50d6705d183218dfbb31f27
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-11 18:32:16 +0900

    Add ft_striteri function

---
+++ ft_striteri.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 17:50:49 by katakada          #+#    #+#             */
+/*   Updated: 2024/07/11 18:31:55 by katakada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	idx;
+
+	idx = 0;
+	if (!s || !f)
+		return ;
+	while (s[idx])
+	{
+		f(idx, &s[idx]);
+		idx++;
+	}
+}
