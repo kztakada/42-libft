89263536ea882d16409d9c554980385c67721711
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-12 19:10:50 +0900

    Add ft_lstadd_front, ft_lstnew, and ft_lstsize functions

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 17:52:11 by katakada          #+#    #+#             */
+/*   Updated: 2024/07/12 19:09:10 by katakada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
