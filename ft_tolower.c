83090d0938980edb13ea40a5ae40d0982d9bd975
Kazuki Takada <katakada@student.42tokyo.jp>
2024-07-24 19:38:15 +0900

    Fix ft_tolower and ft_toupper functions to conform to glibc

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,18 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:19:32 by katakada          #+#    #+#             */
-/*   Updated: 2024/07/06 18:31:04 by katakada         ###   ########.fr       */
+/*   Updated: 2024/07/24 19:31:14 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#ifdef __linux__
+# define ISLINUX 1
+#else
+# define ISLINUX 0
+#endif
 
-int	ft_tolower(int c)
+int	apple_tolower(int c)
 {
 	int	ctolower;
 
@@ -22,4 +27,29 @@ int	ft_tolower(int c)
 		return (ctolower);
 	}
 	return (c);
+}
+
+int	linux_tolower(int c)
+{
+	int	ctolower;
+
+	if (c >= -128 && c < 256)
+	{
+		if (c >= -128 && c < -1)
+			c = c + 256;
+		if (c >= 'A' && c <= 'Z')
+		{
+			ctolower = c - ('A' - 'a');
+			return (ctolower);
+		}
+	}
+	return (c);
+}
+
+int	ft_tolower(int c)
+{
+	if (ISLINUX == 1)
+		return (linux_tolower(c));
+	else
+		return (apple_tolower(c));
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -6,13 +6,18 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 17:00:26 by katakada          #+#    #+#             */
-/*   Updated: 2024/07/06 18:29:20 by katakada         ###   ########.fr       */
+/*   Updated: 2024/07/24 19:34:18 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#ifdef __linux__
+# define ISLINUX 1
+#else
+# define ISLINUX 0
+#endif
 
-int	ft_toupper(int c)
+int	apple_toupper(int c)
 {
 	int	ctoupper;
 
@@ -22,4 +27,29 @@ int	ft_toupper(int c)
 		return (ctoupper);
 	}
 	return (c);
+}
+
+int	linux_toupper(int c)
+{
+	int	ctoupper;
+
+	if (c >= -128 && c < 256)
+	{
+		if (c >= -128 && c < -1)
+			c = c + 256;
+		if (c >= 'a' && c <= 'z')
+		{
+			ctoupper = c + ('A' - 'a');
+			return (ctoupper);
+		}
+	}
+	return (c);
+}
+
+int	ft_toupper(int c)
+{
+	if (ISLINUX == 1)
+		return (linux_toupper(c));
+	else
+		return (apple_toupper(c));
 }
